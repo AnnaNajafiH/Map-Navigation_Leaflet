bf951874d6321f813d45637f0a31bd6504338479
Anna N <nahid.najafi.h@gmail.com>
2024-07-19 09:59:23 +0200

    add loading and error

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,25 @@
-// src/App.jsx
-
 import React, { useEffect, useState } from "react";
 import MapComponent from "./components/MapComponent";
 import { fetchBookshelves } from "./services/bookshelfService";
-// import ManualLocationInput from "./components/ManualLocationInput";
-
 
 const App = () => {
   const [bookshelves, setBookshelves] = useState([]);
   const [center, setCenter] = useState([51.541574, 9.951122]); // Default center
   const [userLocation, setUserLocation] = useState(null);
   const [destination, setDestination] = useState(null);
+  const [loadingLocation, setLoadingLocation] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const getBookshelves = async () => {
       try {
         const data = await fetchBookshelves();
         setBookshelves(data);
-        // console.log("Bookshelves fetched:", data);
-
       } catch (error) {
         console.error("Failed to fetch bookshelves:", error);
       }
     };
-    //Fetching User Location -----------------------------------------
+
     const getUserLocation = () => {
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(
@@ -34,7 +30,7 @@ const App = () => {
             ];
             setCenter(userCoords);
             setUserLocation(userCoords);
-            console.log("User location set:", userCoords);
+            setLoadingLocation(false);
           },
           (error) => {
             if (error.code === error.PERMISSION_DENIED) {
@@ -42,47 +38,44 @@ const App = () => {
               alert(
                 "Location access is required for navigation. Please enable location services or enter your location manually."
               );
-              //  default location
-              const defaultCoords = [51.541574, 9.951122]; // Example default location
-              setCenter(defaultCoords);
-              setUserLocation(defaultCoords);
+              setCenter([51.541574, 9.951122]); // Default location
+              setUserLocation([51.541574, 9.951122]);
             } else {
               console.error("Error getting user location: ", error);
             }
+            setLoadingLocation(false);
           }
         );
       } else {
         console.error("Geolocation is not supported by this browser.");
+        setLoadingLocation(false);
       }
     };
-//-----------------------------------------
+
     getBookshelves();
     getUserLocation();
   }, []);
 
+  if (loadingLocation) {
+    return <div>Loading location...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
-    <div>
-      {/* {!userLocation && (
-        <div>
-          <ManualLocationInput setUserLocation={setUserLocation} />
-        </div>
-      )} */}
-      <MapComponent
-        bookshelves={bookshelves}
-        center={center}
-        userLocation={userLocation}
-        destination={destination}
-        // setDestination={setDestination}
-        setDestination={(loc) => {
-          setDestination(loc);
-          console.log("Destination set:", loc);
-        }}
-      />
-    </div>
+    <MapComponent
+      bookshelves={bookshelves}
+      center={center}
+      userLocation={userLocation}
+      destination={destination}
+      setDestination={(loc) => {
+        setDestination(loc);
+        console.log("Destination set:", loc);
+      }}
+    />
   );
 };
 
 export default App;
-
-
-
