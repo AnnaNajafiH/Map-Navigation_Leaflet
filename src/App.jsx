c4c4cd3ceeb0b026ec2bc03bbf27e99a166c9583
Anna N <nahid.najafi.h@gmail.com>
2024-07-20 15:37:29 +0200

    adding mini map and user location

--- src/App.jsx
+++ src/App.jsx
@@ -12,41 +12,44 @@ const App = () => {
   const [loadingLocation, setLoadingLocation] = useState(true);
   const [error, setError] = useState(null);
 
-const staticBookshelves = [
-  {
-    name: "Jugend Bookshelf",
-    address: "Kochstraße, Leipzig",
-    location: [51.321003, 12.3716],
-    imageUrl:
-      "https://s1.qwant.com/thumbr/474x632/0/8/34ec604c3e1ede755f6e5142afdbc07403c6e8c0269cbd8fb86668b984b940/th.jpg?u=https%3A%2F%2Ftse.mm.bing.net%2Fth%3Fid%3DOIP.YXGuhiA4ebINec46PhhbEgHaJ4%26pid%3DApi&q=0&b=1&p=0&a=0",
-    openingHours: "9:00 AM - 5:00 PM",
-    rating: 4.5,
-    liked: false,
-  },
-  {
-    name: "Central Library",
-    address: "NeumanStraße, Berlin",
-    location: [52.557614, 13.422919],
-    imageUrl:
-      "https://s2.qwant.com/thumbr/474x632/7/7/b517b3db7d8360f12948c8376dc6b9b124d40891de9e2406be5ef894035e96/th.jpg?u=https%3A%2F%2Ftse.mm.bing.net%2Fth%3Fid%3DOIP.Z6wcAr0v4uZ4DijLfS9mOAHaJ4%26pid%3DApi&q=0&b=1&p=0&a=0",
-    openingHours: "9:00 AM - 5:00 PM",
-    rating: 3,
-    liked: false,
-  },
-  {
-    name: "Community Bookshelf",
-    address: "Breubergstraße, Frankfurt am main",
-    location: [50.08557, 8.641062],
-    imageUrl:
-      "https://s2.qwant.com/thumbr/474x460/4/2/8dd6e30a587d4aac6fd7e96f2a7f72647db315f946cb57e9a84ab87d711465/th.jpg?u=https%3A%2F%2Ftse.mm.bing.net%2Fth%3Fid%3DOIP.uaSwTd1xRMlNowxeMX3CygHaHM%26pid%3DApi&q=0&b=1&p=0&a=0",
+  //--------------------------------------------static data---------------------------------------------
+  const staticBookshelves = [
+    {
+      name: "Jugend Bookshelf",
+      address: "Kochstraße, Leipzig",
+      location: [51.321003, 12.3716],
+      imageUrl:
+        "https://s1.qwant.com/thumbr/474x632/0/8/34ec604c3e1ede755f6e5142afdbc07403c6e8c0269cbd8fb86668b984b940/th.jpg?u=https%3A%2F%2Ftse.mm.bing.net%2Fth%3Fid%3DOIP.YXGuhiA4ebINec46PhhbEgHaJ4%26pid%3DApi&q=0&b=1&p=0&a=0",
+      openingHours: "9:00 AM - 5:00 PM",
+      rating: 4.5,
+      liked: false,
+    },
+    {
+      name: "Central Library",
+      address: "NeumanStraße, Berlin",
+      location: [52.557614, 13.422919],
+      imageUrl:
+        "https://s2.qwant.com/thumbr/474x632/7/7/b517b3db7d8360f12948c8376dc6b9b124d40891de9e2406be5ef894035e96/th.jpg?u=https%3A%2F%2Ftse.mm.bing.net%2Fth%3Fid%3DOIP.Z6wcAr0v4uZ4DijLfS9mOAHaJ4%26pid%3DApi&q=0&b=1&p=0&a=0",
+      openingHours: "9:00 AM - 5:00 PM",
+      rating: 3,
+      liked: false,
+    },
+    {
+      name: "Community Bookshelf",
+      address: "Breubergstraße, Frankfurt am main",
+      location: [50.08557, 8.641062],
+      imageUrl:
+        "https://s2.qwant.com/thumbr/474x460/4/2/8dd6e30a587d4aac6fd7e96f2a7f72647db315f946cb57e9a84ab87d711465/th.jpg?u=https%3A%2F%2Ftse.mm.bing.net%2Fth%3Fid%3DOIP.uaSwTd1xRMlNowxeMX3CygHaHM%26pid%3DApi&q=0&b=1&p=0&a=0",
 
-    openingHours: "9:00 AM - 5:00 PM",
-    rating: 4,
-    liked: true,
-  },
-];
+      openingHours: "9:00 AM - 5:00 PM",
+      rating: 4,
+      liked: true,
+    },
+  ];
+  //--------------------------------------------static data---------------------------------------------
+
   useEffect(() => {
-//     // Use static data
+    //     // Use static data
     setBookshelves(staticBookshelves);
 
     const getUserLocation = () => {
@@ -64,9 +67,6 @@ const staticBookshelves = [
           (error) => {
             if (error.code === error.PERMISSION_DENIED) {
               console.error("User denied Geolocation");
-              alert(
-                "Location access is required for navigation. Please enable location services or enter your location manually."
-              );
               const defaultCoords = [51.541574, 9.951122];
               setCenter(defaultCoords);
               setUserLocation(defaultCoords);
@@ -111,6 +111,13 @@ const staticBookshelves = [
 
 export default App;
 //------------------------------------------------------------------------
+
+
+
+
+
+
+
 
 // import React, { useEffect, useState } from "react";
 // import MapComponent from "./components/MapComponent";
--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -4,10 +4,13 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import RoutingMachine from "./RoutingMachine";
+import LocationMarker from "./LocationMarker";
+import MinimapControl from "./MinimapControl";
+
 
 // Define the custom icon
 const customIcon = new L.Icon({
-  iconUrl: "https://img.icons8.com/?size=100&id=13800&format=png&color=000000", // Replace with the path to your custom icon
+  iconUrl: "https://img.icons8.com/?size=100&id=13800&format=png&color=000000", 
   iconSize: [32, 32],
   iconAnchor: [16, 32],
   popupAnchor: [0, -32],
@@ -24,9 +27,13 @@ const MapComponent = ({
     <MapContainer
       center={center}
       zoom={5}
+      scrollWheelZoom={true}
       style={{ height: "100vh", width: "100%" }}
     >
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
       {bookshelves.map((shelf, idx) => (
         <Marker key={idx} position={shelf.location} icon={customIcon}>
           <Popup>
@@ -50,9 +57,11 @@ const MapComponent = ({
           </Popup>
         </Marker>
       ))}
+      <LocationMarker />
       {userLocation && destination && (
         <RoutingMachine start={userLocation} end={destination} />
       )}
+      <MinimapControl position="topright" zoom={0} />{" "}
     </MapContainer>
   );
 };
